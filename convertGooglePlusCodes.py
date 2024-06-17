7de0726f6707f3319342cde3a1a6bdcaf3785190
prasolay2020 <prasolay2020@gmail.com>
2024-06-17 10:57:44 +0800

    增加eid完成

--- convertGooglePlusCodes.py
+++ convertGooglePlusCodes.py
@@ -31,8 +31,74 @@ df1 = df1.dropna(subset=['lon', 'lat'])
 
 df1["googlePlus"] = df1.apply(lambda row: olc.encode(row['lat'], row['lon']), 'columns')
 # df1["googlePlus"] =  olc.encode(df1['lat'], df1['lon'])
-print(df1["googlePlus"])
+print(df1["googlePlus"][1])
+print("--------------")
 # olc.encode(1.2867970586693043, 103.85451499941605)
 
-#output to csv
-df1.to_csv("data\\output\\GPC_Tainan110.csv", encoding="UTF-8")
+#add eid
+lut_county_byname = {
+    '臺北市': ['A', 'Taipei'],
+    '新北市': ['F', 'NewTaipei'],
+    '基隆市': ['C', 'Keelung'],
+    '桃園市': ['H', 'Taoyuan'],
+    '新竹市': ['O', 'Hsinchu'],
+    '新竹縣': ['J', 'Hsinchu'],
+    '苗栗縣': ['K', 'Miaoli'],
+    '臺中市': ['B', 'Taichung'],
+    '彰化縣': ['N', 'Changhua'],
+    '南投縣': ['M', 'Nantou'],
+    '雲林縣': ['P', 'Yunlin'],
+    '嘉義縣': ['Q', 'Chiayi'],
+    '嘉義市': ['I', 'Chiayi'],
+    '臺南市': ['D', 'Tainan'],
+    '高雄市': ['E', 'Kaohsiung'],
+    '屏東縣': ['T', 'Pingtung'],
+    '宜蘭縣': ['G', 'Ilan'],
+    '花蓮縣': ['U', 'Hualien'],
+    '臺東縣': ['V', 'Taitung'],
+    '澎湖縣': ['X', 'Penghu'],
+    '金門縣': ['W', 'Jinmen'],
+    '連江縣': ['Z', 'Lianjiang']
+}
+
+def createEid(inputDF, countyName, year):
+
+    lut_county_byname = {
+    '臺北市': ['A', 'Taipei'],
+    '新北市': ['F', 'NewTaipei'],
+    '基隆市': ['C', 'Keelung'],
+    '桃園市': ['H', 'Taoyuan'],
+    '新竹市': ['O', 'Hsinchu'],
+    '新竹縣': ['J', 'Hsinchu'],
+    '苗栗縣': ['K', 'Miaoli'],
+    '臺中市': ['B', 'Taichung'],
+    '彰化縣': ['N', 'Changhua'],
+    '南投縣': ['M', 'Nantou'],
+    '雲林縣': ['P', 'Yunlin'],
+    '嘉義縣': ['Q', 'Chiayi'],
+    '嘉義市': ['I', 'Chiayi'],
+    '臺南市': ['D', 'Tainan'],
+    '高雄市': ['E', 'Kaohsiung'],
+    '屏東縣': ['T', 'Pingtung'],
+    '宜蘭縣': ['G', 'Ilan'],
+    '花蓮縣': ['U', 'Hualien'],
+    '臺東縣': ['V', 'Taitung'],
+    '澎湖縣': ['X', 'Penghu'],
+    '金門縣': ['W', 'Jinmen'],
+    '連江縣': ['Z', 'Lianjiang']
+    }
+
+    countyCodeName = lut_county_byname[countyName][0]
+    for index,  googlePlus in enumerate(inputDF["googlePlus"]):
+        inputDF.at[index, "eid"] = countyCodeName + "-" + year + "-" + str(index+1).zfill(5)
+    
+    return inputDF
+
+    
+df2 = createEid(df1, '臺南市', '110')
+
+print(df2.loc[0:1])
+print("-------------")
+
+# #output to csv
+# df1.to_csv("data\\output\\GPC_Tainan110.csv", encoding="big5")
