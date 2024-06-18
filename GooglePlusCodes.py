765ff3fd2d41c96d27e932963ed0530f23b968f0
prasolay2020 <prasolay2020@gmail.com>
2024-06-18 15:39:12 +0800

    測試完成

--- GooglePlusCodes.py
+++ GooglePlusCodes.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 def codeDigit(inputNmuber):
     google_plus_code_dict = {
@@ -27,6 +28,9 @@ def codeDigit(inputNmuber):
 
     return outputnumber
 
+def calculateNmuber(inputNumber):
+    
+
 
 
 
@@ -35,3 +39,38 @@ def codeDigit(inputNmuber):
 
 if __name__ == '__main__':
     print("hello world!")
+    print("-----------------------------")
+
+    #測試區域-魚尾獅噴泉 (北緯1.286785°，東經103.854503°。）
+    #1.2867970586693043, 103.85451499941605
+    #正確的回傳資料：6PH57VP3+PR
+    yo = 1.2867970586693043
+    xo = 103.8545149994160
+
+    y1 = (math.floor((90+yo)/20))
+    print("y1:")
+    print(y1)
+    print("-----------------------------")
+    x1 = (math.floor((180+xo)/20))
+    print("x1:")
+    print(x1)
+
+    print("-----------------------------")
+    print("y2:")
+    y2 = math.floor(((90+yo)-y1*20)/1)
+    print(y2)
+
+    print("-----------------------------")
+    print("y3:")
+    y3 = math.floor((((90+yo)-y1*20)-y2*1)/0.05)
+    print(y3)
+
+    print("-----------------------------")
+    print("y4:")
+    y4 = math.floor(((((90+yo)-y1*20)-y2*1-y3*0.05)/0.0025))
+    print(y4)
+
+    print("-----------------------------")
+    print("y5:")
+    y5 = ((((90+yo)-y1*20)-y2*1-y3*0.05-y4*0.0025)/0.000125)
+    print(y5)
